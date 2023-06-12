bf6b7c0fc66a7d612600c947d144d22ca7681801
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-06-12 16:49:08 +0300

    feat: added domain models for Authentication and Registration commands FQW-Users-2

---
+++ Users.Domain/Authentication/Requests/Commands/AuthenticationCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+using Users.Domain.Authentication.Responses.Commands;
+
+namespace Users.Domain.Authentication.Requests.Commands;
+
+public class AuthenticationCommand : IRequest<AuthenticationResponse>
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
